bbe1cac698d2be9fc644eb5ddf65b9c0da452338
Usmon <nuriddinzoda882222431@gmainl.com>
2022-02-21 00:09:45 +0500

    first commit

---
+++ UIDesing/MainViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace UIDesing
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<Places> Places;
+
+        public ObservableCollection<Places> places
+        {
+            get { return Places; }
+            set
+            {
+                Places = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("places"));
+            }
+        }
+
+        public MainViewModel()
+        {
+            places = new ObservableCollection<Places>();
+            addDate();
+        }
+
+        private void addDate()
+        {
+            places.Add(new Places
+            {
+                Id = 0,
+                Title = "The beautiful sunset",
+                Country = "Franche",
+                ImgSource = "https://bestwallpapers.in/wp-content/uploads/2018/04/palm-trees-sunset-orange-sun-green-water-4k-wallpaper-1440x2560.jpg"
+            });
+            places.Add(new Places
+            {
+                Id = 1,
+                Title = "The best places",
+                Country = "Tajikistan",
+                ImgSource = "https://www.3wallpapers.fr/wp-content/uploads/2017/09/iphone_wallpaper_sunset_sky.jpg"
+            });
+            places.Add(new Places
+            {
+                Id = 2,
+                Title = "The Beautiful",
+                Country = "Russia",
+                ImgSource = "https://ihdwall.com/storage/202101/sunset-road-landscape-scenery-hd-phone-wallpaper-1125x2436.jpg"
+            });
+            places.Add(new Places
+            {
+                Id = 3,
+                Title = "So match ",
+                Country = "USA",
+                ImgSource = "https://i.pinimg.com/originals/66/55/03/6655039bd090abed0037746c6edf662b.jpg"
+            });
+
+        }
+    }
+}
---
+++ UIDesing/Places.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UIDesing
+{
+    public class Places
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Country { get; set; }
+        public string ImgSource  { get; set; }
+    }
+}
